1ab69ba8c914c01078579c4bfded6c28d283ab82
kateschlagel <kmaillette@gmail.com>
2014-06-17 21:20:09 -0500

    Added small php script to send email data to Rosa's inbox.

---
+++ sendmail.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: kateschlagel
+ * Date: 6/17/14
+ * Time: 8:10 PM
+ */
+
+$name = $_POST['name'];
+
+$to = 'kmaillette@gmail.com';
+$subject = 'New Subscriber';
+
+$message = "Name: $name\n";
+$message .= 'Email: ' . $_POST['email'] . "\n";
+$message .= "\n";
+
+$headers =	'From: <' . $_POST['email'] . '> '. $name ."\r\n" .
+    'Reply-To: <' . $_POST['email'] . '> '. $name . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+
+mail($to, $subject, $message, $headers);
+header('location: http://localhost:8888/RCon/');
+?>
